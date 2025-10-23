9f7cacd8288f583b813d0aaf95214f80948dc041
M Obaydullah <52800314+obaydullahmhs@users.noreply.github.com>
2025-10-23 12:47:03 +0600

    Add rule name for all routes (#6)
    Signed-off-by: obaydullahmhs <obaydullah@appscode.com>

--- apis/gateway/v1alpha1/redisroute_types.go
+++ apis/gateway/v1alpha1/redisroute_types.go
@@ -124,6 +124,11 @@ type RedisRouteStatus struct {
 
 // RedisRouteRule is the configuration for a given rule.
 type RedisRouteRule struct {
+	// Name is the name of the route rule. This name MUST be unique within a Route if it is set.
+	//
+	// Support: Extended
+	// +optional
+	Name *gwv1.SectionName `json:"name,omitempty"`
 	// Filters define the filters that are applied to requests that match
 	// this rule.
 	//
